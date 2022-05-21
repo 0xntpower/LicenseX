134ea2b44982e4438244dc95a2196cf55044a40f
Nort721 <itag7894@gmail.com>
2022-05-21 12:16:52 +0300

    improvements

--- LicenseManagerX/src/main/java/dev/licensex/manager/gui/MainGUI.java
+++ LicenseManagerX/src/main/java/dev/licensex/manager/gui/MainGUI.java
@@ -2,49 +2,46 @@ package dev.licensex.manager.gui;
 
 import javax.swing.*;
 import javax.swing.border.BevelBorder;
+import javax.swing.JMenuBar;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class MainGUI {
-    private JFrame jFrame;
-    private JProgressBar bar;
+public class MainGUI extends JFrame {
 
     public MainGUI() {
         buildWindow();
-        jFrame.show();
+        show();
     }
 
     private void buildWindow() {
-        jFrame = new JFrame();
+        setResizable(true);
+        setTitle("LicenseManagerX");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(200, 200, 1100, 680);
+        setLayout(new BorderLayout());
 
-        jFrame.setResizable(true);
-        jFrame.setTitle("LicenseManagerX");
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jFrame.setBounds(200, 200, 1100, 680);
-        jFrame.setLayout(new BorderLayout());
+        JMenuBar menuBar = utils.getMenuBar();
 
-        JMenuBar menuBar = getMenuBar();
+        getContentPane().add(menuBar, BorderLayout.PAGE_START);
 
-        jFrame.getContentPane().add(menuBar, BorderLayout.PAGE_START);
-
-        JPanel panel1 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JPanel leftPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         JLabel label = new JLabel();
         label.setHorizontalAlignment(JLabel.LEFT);
-        panel1.setBorder(new BevelBorder(BevelBorder.LOWERED));
-        panel1.setPreferredSize(new Dimension(jFrame.getWidth() / 2, jFrame.getHeight() - (menuBar.getHeight() + 65)));
-        panel1.add(label);
+        leftPanel.setBorder(new BevelBorder(BevelBorder.LOWERED));
+        leftPanel.setPreferredSize(new Dimension(getWidth() / 2, getHeight() - (menuBar.getHeight() + 65)));
+        leftPanel.add(label);
 
-        JPanel panel2 = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-        bar = new JProgressBar();
+        JPanel rightPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        JProgressBar bar = new JProgressBar();
         bar.setStringPainted(true);
         bar.setOpaque(false);
         bar.setVisible(false);
-        panel2.setBorder(new BevelBorder(BevelBorder.LOWERED));
-        panel2.setPreferredSize(new Dimension(jFrame.getWidth() / 3, jFrame.getHeight() - (menuBar.getHeight() + 65)));
-        panel2.add(bar);
+        rightPanel.setBorder(new BevelBorder(BevelBorder.LOWERED));
+        rightPanel.setPreferredSize(new Dimension(getWidth() / 3, getHeight() - (menuBar.getHeight() + 65)));
+        rightPanel.add(bar);
 
-        JSplitPane spt = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, panel1, panel2) {
+        JSplitPane spt = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, leftPanel, rightPanel) {
             private final int location = 250;
 
             {
@@ -62,82 +59,87 @@ public class MainGUI {
             }
         };
 
-        jFrame.add(spt, BorderLayout.SOUTH);
+        add(spt, BorderLayout.SOUTH);
     }
 
-    private JMenuBar getMenuBar() {
-        JMenuBar menuBar = new JMenuBar();
+    static class utils {
 
-        JMenu productMenu = new JMenu("Product");
+        public static JMenuBar getMenuBar() {
+            JMenuBar menuBar = new JMenuBar();
 
-        JMenuItem newProductMenuItem = new JMenuItem("New product");
-        newProductMenuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+            JMenu productMenu = new JMenu("Product");
 
-            }
-        });
-        productMenu.add(newProductMenuItem);
+            JMenuItem newProductMenuItem = new JMenuItem("New product");
+            newProductMenuItem.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
 
-        JMenuItem newProductCategoryMenuItem = new JMenuItem("New product category");
-        newProductCategoryMenuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+                }
+            });
+            productMenu.add(newProductMenuItem);
 
-            }
-        });
-        productMenu.add(newProductCategoryMenuItem);
+            JMenuItem newProductCategoryMenuItem = new JMenuItem("New product category");
+            newProductCategoryMenuItem.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
 
-        menuBar.add(productMenu);
+                }
+            });
+            productMenu.add(newProductCategoryMenuItem);
 
-        JMenu licenseMenu = new JMenu("License");
+            menuBar.add(productMenu);
 
-        JMenuItem generateLicenseMenuItem = new JMenuItem("Generate New License");
-        generateLicenseMenuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+            JMenu licenseMenu = new JMenu("License");
 
-            }
-        });
-        licenseMenu.add(generateLicenseMenuItem);
+            JMenuItem generateLicenseMenuItem = new JMenuItem("Generate New License");
+            generateLicenseMenuItem.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
 
-        menuBar.add(licenseMenu);
+                }
+            });
+            licenseMenu.add(generateLicenseMenuItem);
 
-        JMenu ThemesMenu = new JMenu("Themes");
+            menuBar.add(licenseMenu);
 
-        JMenuItem darkModeMenuItem = new JMenuItem("Dracula");
-        darkModeMenuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+            JMenu ThemesMenu = new JMenu("Themes");
 
-            }
-        });
-        ThemesMenu.add(darkModeMenuItem);
+            JMenuItem darkModeMenuItem = new JMenuItem("Dracula");
+            darkModeMenuItem.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
 
-        menuBar.add(ThemesMenu);
+                }
+            });
+            ThemesMenu.add(darkModeMenuItem);
 
-        JMenu HelpMenu = new JMenu("Help");
+            menuBar.add(ThemesMenu);
 
-        JMenuItem checkForUpdatesMenuItem = new JMenuItem("Check For Updates");
-        checkForUpdatesMenuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+            JMenu HelpMenu = new JMenu("Help");
 
-            }
-        });
-        HelpMenu.add(checkForUpdatesMenuItem);
+            JMenuItem checkForUpdatesMenuItem = new JMenuItem("Check For Updates");
+            checkForUpdatesMenuItem.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
 
-        JMenuItem aboutMenuItem = new JMenuItem("About");
-        aboutMenuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+                }
+            });
+            HelpMenu.add(checkForUpdatesMenuItem);
 
-            }
-        });
-        HelpMenu.add(aboutMenuItem);
+            JMenuItem aboutMenuItem = new JMenuItem("About");
+            aboutMenuItem.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
 
-        menuBar.add(HelpMenu);
+                }
+            });
+            HelpMenu.add(aboutMenuItem);
 
-        return menuBar;
+            menuBar.add(HelpMenu);
+
+            return menuBar;
+        }
+
     }
+
 }
