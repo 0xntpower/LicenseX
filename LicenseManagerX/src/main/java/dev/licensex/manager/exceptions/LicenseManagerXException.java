9cb4341f35d2585857a9754b0db5848753a91583
Nort721 <itag7894@gmail.com>
2022-07-16 14:43:50 +0300

    exceptions & network design

---
+++ LicenseManagerX/src/main/java/dev/licensex/manager/exceptions/LicenseManagerXException.java
@@ -0,0 +1,7 @@
+package dev.licensex.manager.exceptions;
+
+public class LicenseManagerXException extends Exception {
+    public LicenseManagerXException(String msg) {
+        super(msg);
+    }
+}
