34ef789693b0d26a11126f7ef8c538f104ad835b
Nort721 <itag7894@gmail.com>
2022-07-30 19:50:25 +0300

    implemented search GUI design code

--- LicenseManagerX/src/main/java/dev/licensex/manager/gui/pages/SearchLicenseGUI.java
+++ LicenseManagerX/src/main/java/dev/licensex/manager/gui/pages/SearchLicenseGUI.java
@@ -3,7 +3,8 @@ package dev.licensex.manager.gui.pages;
 import dev.licensex.manager.Launcher;
 
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SearchLicenseGUI extends JFrameX {
     private static boolean isRunning;
@@ -17,27 +18,48 @@ public class SearchLicenseGUI extends JFrameX {
 
     @Override
     protected void buildWindow() {
+        String os = System.getProperty("os.name");
+
         setTitle("Search License");
-        setBounds(420, 150, 420, 130);
+        setBounds(400, 280, 380, 127);
         setLocationRelativeTo(Launcher.mainGUI);
-        setLayout(new BorderLayout());
 
         JPanel panel = new JPanel();
         panel.setLayout(null);
 
-        // Section: license details
-        // - original license id
-        // - current license id
-        // - date when generated
+        JTextField searchTextField = new JTextField();
+        searchTextField.setBounds(55, 10, 250, 19);
+        searchTextField.setColumns(10);
+        panel.add(searchTextField);
 
-        // Section: license properties
-        // type: per-machine/concurrent-sessions/unlimited
-        // type-limit: number limit here
-        // expiration: date/Lifetime
+        JButton btnSave = new JButton("Search");
+        btnSave.setBounds(90, 43, 180, 20);
+        panel.add(btnSave);
+        btnSave.addActionListener(new ActionListener() {
 
-        // Section: User data
-        // Registered machines: amount-of-machine  [show]
-        // Ip history: amount-of-ips  [show]
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (searchTextField.getText().length() == 0) {
+                    showDialog("Please type in a license-id", "Process failed!", JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+
+                // ToDo search a license and expand the explorer and select it
+
+                dispose();
+            }
+        });
+
+        JButton btnCancel = new JButton("Cancel");
+        btnCancel.setBounds(90, os.contains("Mac OS X") ? 66 : 63, 180, 20);
+        panel.add(btnCancel);
+        btnCancel.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
 
         setContentPane(panel);
     }
