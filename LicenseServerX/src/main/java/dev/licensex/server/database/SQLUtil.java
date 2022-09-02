2a46be8b0a26e9796b43257ada23a58abc895d25
Nort721 <itag7894@gmail.com>
2022-09-02 19:09:49 +0300

    Update SQLUtil.java

--- LicenseServerX/src/main/java/dev/licensex/server/database/SQLUtil.java
+++ LicenseServerX/src/main/java/dev/licensex/server/database/SQLUtil.java
@@ -7,6 +7,11 @@ public class SQLUtil {
 
     public static void saveProduct(String name, String category) {
         // ToDo first check if a product with the same name and category already exists
+
+        String GET_PRODUCT_NAME_QUERY = "SELECT proT.Name " +
+                "FROM TestDB.dbo.Products proT " +
+                "WHERE proT.Name = '" + name + "' AND proT.Category = '" + category + "'";
+
         String SAVE_PRODUCT_QUERY = "INSERT INTO TestDB.dbo.Products(Name, Category) " +
                 "VALUES ('" + name + "', '" + category + "');";
     }
@@ -16,7 +21,11 @@ public class SQLUtil {
     }
 
     public static void saveLicense(String licenseText, String productName, String categoryName) {
+        // ToDo first check if a license with the same licenseText already exists
 
+        String GET_LICENSE_TEXT_QUERY = "";
+
+        String SAVE_LICENSE_QUERY = "";
     }
 
     public static void deleteLicense(String licenseText, String productName, String categoryName) {
@@ -25,6 +34,14 @@ public class SQLUtil {
 
     public static void saveUsageHistory(String licenseText, String ipAddress, String hwid) {
 
+        String GET_LICENSE_ID_QUERY = "SELECT Id FROM TestDB.dbo.Licenses " +
+                "WHERE LicenseText = '" + licenseText + "'";
+
+        String license_id = ""; // result from GET_LICENSE_ID_QUERY here
+
+        String SAVE_USAGE_HISTORY_QUERY = "INSERT INTO TestDB.dbo.Usage_history(License_id, IP, HWID) " +
+                "VALUES (" + license_id + ", '" + ipAddress + "', '" + hwid + "');";
+
     }
 
     public static void deleteUsageHistory(String licenseText, String ipAddress, String hwid) {
@@ -32,10 +49,23 @@ public class SQLUtil {
     }
 
     public static String getLicenseData(String licenseText) {
+
+        String GET_LICENSE_DATA_QUERY = "SELECT licT.Id, licT.LicenseText, proT.Name as Product, proT.Category " +
+                "FROM TestDB.dbo.Licenses licT JOIN TestDB.dbo.Products proT " +
+                "ON licT.Product = proT.Id " +
+                "WHERE licT.LicenseText = '" + licenseText + "'";
+
+        // ToDO check if the license actually exists, otherwise the query will return empty data probably?
+
         return "";
     }
 
     public static boolean shouldLicenseActivate(String licenseText) {
+
+        // this will fail to return the text if the license doesn't exist, otherwise it exists
+        String GET_LICENSE_TEXT_QUERY = "SELECT LicenseText FROM TestDB.dbo.Licenses " +
+                "WHERE LicenseText = '" + licenseText + "'";
+
         return true;
     }
 }
