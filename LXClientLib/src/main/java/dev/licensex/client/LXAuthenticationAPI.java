c2fb23bd71e3c8a0787e75db8a52b1a5f9a38578
Nort721 <itag7894@gmail.com>
2023-01-17 18:48:29 +0200

    Update LXAuthenticationAPI.java

---
+++ LXClientLib/src/main/java/dev/licensex/client/LXAuthenticationAPI.java
@@ -0,0 +1,5 @@
+package dev.licensex.client;
+
+public class LXAuthenticationAPI {
+
+}
--- LXClientLib/src/main/java/dev/licensex/client/Launcher.java
+++
@@ -1,8 +0,0 @@
-package dev.licensex.client;
-
-public final class Launcher {
-
-    public static void main(String[] args) {
-        
-    }
-}
